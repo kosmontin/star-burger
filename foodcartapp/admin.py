b369270271333b0750fb93f8c80014a45b3e84f7
Konstantin Beloborodov <kosta.white@gmail.com>
2022-07-06 12:33:31 +0300

    add redirect for manager orders page from order edit

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
+from star_burger import settings
 from .models import Product
 from .models import ProductCategory
 from .models import Restaurant
@@ -132,3 +135,11 @@ class OrderAdmin(admin.ModelAdmin):
     @admin.display(description='Номер телефона')
     def get_phonenumber(self, obj):
         return obj.client.phonenumber
+
+    def response_post_save_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        if 'next' in request.GET and url_has_allowed_host_and_scheme(
+                request.GET['next'], settings.ALLOWED_HOSTS):
+            return HttpResponseRedirect(request.GET['next'])
+        else:
+            return res
