8951fc600737157d104a92d8a50c535dffed211b
Konstantin Beloborodov <kosta.white@gmail.com>
2022-09-09 09:25:08 +0300

    add minor changes

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
 YANDEX_GEO_APIKEY = env('YANDEX_GEO_APIKEY', None)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['192.168.100.11', '127.0.0.1', 'localhost', 'home.mradmin.ru'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
@@ -119,7 +119,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 INTERNAL_IPS = [
-    '127.0.0.1'
+    '127.0.0.1',
+    '192.168.100.11',
 ]
 
 
