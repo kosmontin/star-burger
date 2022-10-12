22ba6280010e0f408e67030e0c2e750c0b33e5ad
Konstantin Beloborodov <kosta.white@gmail.com>
2022-10-12 17:26:54 +0300

    modify deploy script

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+param=$1
 set -e
 echo "Trying to rebuild star-burger project..."
 cd /opt/projects/star-burger/
@@ -12,13 +13,6 @@ then
 echo "Nothing to update"
 exit 0
 fi
-param=$1
-if [[ $param != ''  ]]
-then
-commit=$(git rev-parse --short HEAD)
-token='X-Rollbar-Access-Token: '$param
-curl --request POST --url https://api.rollbar.com/api/1/deploy --header $token --header 'accept: application/json' --header 'content-type: application/json' --data '{"environment": "production", "revision": $commit, "status": "started"}'
-fi
 . ./venv/bin/activate
 pip install -r requirements.txt
 npm ci
@@ -28,8 +22,10 @@ npm ci
 sudo systemctl restart gunicorn.service
 sudo systemctl reload nginx.service
 deactivate
-if [[ $param != ''  ]]
+if [[ $param != '' ]]
 then
+commit=$(git rev-parse --short HEAD)
+token='X-Rollbar-Access-Token: '$param
 curl --request POST --url https://api.rollbar.com/api/1/deploy --header $token --header 'accept: application/json' --header 'content-type: application/json' --data '{"environment": "production", "revision": $commit, "status": "succeeded"}'
 fi
 echo "Rebuild project completed"
